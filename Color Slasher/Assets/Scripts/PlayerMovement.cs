0feb875782fe7c1897a7f561147c13e736186d5b
NothingBox <jose.magudelo@hotmail.com>
2020-07-24 14:58:24 -0500

    MovementMechanic V.idk
    Grab it.

--- Color Slasher/Assets/Scripts/PlayerMovement.cs
+++ Color Slasher/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //  Force applied to the slash movement.
-    [SerializeField] float forceSwipe = 10f, frictionSwipe = 0.1f;
+    //  Force applied to the slash movement | 
+    //  Friction applied to the slash (contrary to DirectionSwipe) | 
+    //  Seconds the character remains without falling when touches a platform.
+    [SerializeField] float forceSwipe = 10f, frictionSwipe = 1f, secondsGrab = 3f;
+
+    //  Debug variables. Code works with Properties.
+    public bool canSwipe, doubleSwipe, falling;
 
     Rigidbody2D rb;
     Vector3 startPos, finalPos;
@@ -14,14 +19,19 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 
-        DirectionSwipe = Vector2.zero;
+        DirectionSlash = Vector2.zero;
 
-        IsGrounded = true;
+        IsGrounded = false;
+        IsFalling = true;
+
         DoubleSwipe = false;
     }
 
     void FixedUpdate()
     {
+        //  Toggles the gravity depending on wheater the character is moving.
+        rb.gravityScale = (IsFalling)? 1:ActivateFriction();
+
         //  Gets the position where the screen is first touched.
         if(Input.GetButtonDown("Fire1"))
         {
@@ -29,43 +39,62 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //  Gets the position where the screen is last touched.
-        //  Let the player swipe if the character isGrounded (or gets the DoubleSwipe bonus).
+        //  ACTIVATES SLASH
+        //  Let the character slash if isGrounded (or gets the DoubleSwipe bonus).
         if((Input.GetButtonUp("Fire1")) && (IsGrounded || DoubleSwipe))
         {
+            //StopAllCoroutines();
+            IsGrounded = false;
+            IsFalling = false;
             finalPos = Input.mousePosition;
 
-            //  Calculate the distance between x and y components, and traduce it to values from 0 to 1. 
+            //  Calculate the distance between x and y components, then traduces it to values from 0 to 1. 
             Vector3 v = Vector3.Normalize(finalPos - startPos);
             //  Crops the Vector3 to Vector2
-            DirectionSwipe = new Vector2(v.x, v.y);
-
-            IsGrounded = false;
+            DirectionSlash = new Vector2(v.x, v.y);
 
             //  Impulses this object in slash's direction.
-            rb.AddForce(DirectionSwipe * forceSwipe, ForceMode2D.Impulse);
+            rb.AddForce(DirectionSlash * forceSwipe, ForceMode2D.Impulse);
         }
-        
-        //  Toggles the gravity depending on wheater the character is moving and collides against a wall.
-        rb.gravityScale = (IsGrounded)? 1:ActivateFriction();
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        IsGrounded = true;
     }
 
     int ActivateFriction()
     {
-        //  Aplies a force in the contrary direction of the swipe.
-        rb.AddForce(-DirectionSwipe * (forceSwipe * frictionSwipe), ForceMode2D.Force);
+        //  Friction will be applied when character not IsGrounded.
+        if(!IsGrounded) 
+        {
+            //  Applies a force contrary to DirectionSlash's direction.
+            rb.AddForce(-DirectionSlash * (forceSwipe * frictionSwipe), ForceMode2D.Force);
 
-        //  If the characters begins to fall, activate the gravity.
-        if(rb.velocity.y <= 0) IsGrounded = true;
+            //  If the character loses all it's upping energy, start to fall.
+            if(rb.velocity.y <= 0) IsFalling = true;
+        }
 
         return 0;
     }
 
-    public bool IsGrounded {get; set;}
-    public bool DoubleSwipe {get; set;}
-    Vector2 DirectionSwipe {get; set;}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //  Whenever touches a paltform can slash again.
+        IsGrounded = true;
+
+        StartCoroutine(Grab());
+    }
+
+    //  When touches a platform doesn't fall de inmediaty.
+    IEnumerator Grab()
+    {
+        //  Stops whet get to a platform.
+        rb.velocity = Vector2.zero;
+
+        // Waits certain time to fall.
+        yield return new WaitForSeconds(secondsGrab);
+
+        IsFalling = true;
+    }
+
+    public bool IsGrounded {get => canSwipe; set => canSwipe = value;}
+    public bool IsFalling {get => falling; set => falling = value;}
+    public bool DoubleSwipe {get => doubleSwipe; set => doubleSwipe = value;}
+    Vector2 DirectionSlash {get; set;}
 }
