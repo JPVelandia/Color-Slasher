3847e47ba826808276e58870ba673e70e6669229
MariSerna371 <68366236+MariSerna371@users.noreply.github.com>
2020-07-23 23:31:51 -0500

    ColorMechanic
    Programming of the color mechanic

---
+++ Color Slasher/Assets/Scripts/ColorMechanic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ColorMech { blue, red, yellow, green, white }
+
+public class ColorMechanic : MonoBehaviour
+{
+    SpriteRenderer sr;
+    ColorMech cm;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Blue Platform")
+        {
+            sr.color = Color.blue;
+            cm = ColorMech.blue;
+        }
+        if (collision.gameObject.name == "Red Platform")
+        {
+            sr.color = Color.red;
+            cm = ColorMech.red;
+        }
+        if (collision.gameObject.name == "Yellow Platform")
+        {
+            sr.color = Color.yellow;
+            cm = ColorMech.yellow;
+        }
+        if (collision.gameObject.name == "Green Platform")
+        {
+            sr.color = Color.green;
+            cm = ColorMech.green;
+        }
+        if (collision.gameObject.name == "Floor")
+        {
+            sr.color = Color.white;
+            cm = ColorMech.white;
+        }
+    }
+}
