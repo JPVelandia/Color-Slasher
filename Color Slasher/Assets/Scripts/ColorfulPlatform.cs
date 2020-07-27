ca91d5cce819ff3ac04e9bede9460a32d0044cd1
MariSerna371 <68366236+MariSerna371@users.noreply.github.com>
2020-07-26 19:57:22 -0500

    Colorful Platform Correction
    Corrected color mechanic so that numbers would not be repeated once in a row

--- Color Slasher/Assets/Scripts/ColorfulPlatform.cs
+++ Color Slasher/Assets/Scripts/ColorfulPlatform.cs
@@ -7,6 +7,7 @@ public class ColorfulPlatform : MonoBehaviour
     SpriteRenderer sr;
     System.Random rnd = new System.Random();
     [SerializeField] int state;
+    bool blue = false, red = false, yellow = false, green = false;
 
     void Start()
     {
@@ -23,48 +24,96 @@ public class ColorfulPlatform : MonoBehaviour
     {
         sr.color = Color.blue;
         gameObject.name = "Blue Platform";
+        blue = true;
+        red = false;
+        yellow = false;
+        green = false;
         Invoke("RandomNumber", 3f);
     }
     public void RedState()
     {
         sr.color = Color.red;
         gameObject.name = "Red Platform";
+        blue = false;
+        red = true;
+        yellow = false;
+        green = false;
         Invoke("RandomNumber", 3f);
     }
     public void YellowState()
     {
         sr.color = Color.yellow;
         gameObject.name = "Yellow Platform";
+        blue = false;
+        red = false;
+        yellow = true;
+        green = false;
         Invoke("RandomNumber", 3f);
     }
-    public void GreennState()
+    public void GreenState()
     {
         sr.color = Color.green;
         gameObject.name = "Green Platform";
+        blue = false;
+        red = false;
+        yellow = false;
+        green = true;
         Invoke("RandomNumber", 3f);
     }
     public void RandomNumber()
     {
         state = rnd.Next(1, 5);
+        Debug.Log(state);
         ChangeState();
     }
     public void ChangeState()
     {
-        if (state == 1)
+        if(blue == false)
         {
-            BlueState();
+            if (state == 1)
+            {
+                BlueState();
+            }
         }
-        if (state == 2)
+        else if(blue == true && state == 1)
         {
-            RedState();
+            RandomNumber();
         }
-        if (state == 3)
+
+        if(red == false)
         {
-            YellowState();
+            if (state == 2)
+            {
+                RedState();
+            }
         }
-        if (state == 4)
+        else if (red == true && state == 2)
         {
-            GreennState();
+            RandomNumber();
+        }
+
+        if (yellow == false)
+        {
+            if (state == 3)
+            {
+                YellowState();
+            }
+        }
+        else if (yellow == true && state == 3)
+        {
+            RandomNumber();
+        }
+
+        if (green == false)
+        {
+            if (state == 4)
+            {
+                GreenState();
+            }
+        }
+        else if (green == true && state == 4)
+        {
+            RandomNumber();
         }
     }
 }
