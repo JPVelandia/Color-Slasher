5a7d2db61d595ecb20a5c1bc515f21e1af1af262
JPVelandia <46972093+JPVelandia@users.noreply.github.com>
2020-07-28 22:39:21 -0500

    Finish
    We made the first level with the winning condition

---
+++ Color Slasher/Assets/Scripts/Platforms/Finish.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField]
+    GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0;
+            winText.SetActive(true);
+        }
+    }
+}
