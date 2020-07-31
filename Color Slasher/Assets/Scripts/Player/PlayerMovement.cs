51d27d361878737bfbc7d1f2899e3f41773e46cc
JPVelandia <46972093+JPVelandia@users.noreply.github.com>
2020-07-31 14:28:54 -0500

    Movimient updated

--- Color Slasher/Assets/Scripts/Player/PlayerMovement.cs
+++ Color Slasher/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(ParticleSystem))]
 public class PlayerMovement : MonoBehaviour
 {
     //  Force applied to the slash movement | 
     //  Friction applied to the slash (contrary to DirectionSwipe) | 
     //  Seconds the character remains without falling when touches a platform.
-    [SerializeField] float forceSwipe = 10f, frictionSwipe = 1f, secondsGrab = 3f;
+    [SerializeField] float forceSlash = 10f, frictionSlash = 1f, secondsGrab = 3f;
 
     //  Debug variables. Code works with Properties.
-    public bool canSwipe, doubleSwipe, falling;
+    public bool canSwipe, onFloor, falling, doubleSwipe;
 
     Rigidbody2D rb;
     Vector3 startPos, finalPos;
+    ParticleSystem particleSlash;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        particleSlash = GetComponentInChildren<ParticleSystem>();
 
         DirectionSlash = Vector2.zero;
 
         IsDead = false;
         IsGrounded = false;
+        IsStill = false;
         IsFalling = true;
 
         DoubleSwipe = false;
@@ -30,32 +35,20 @@ public class PlayerMovement : MonoBehaviour
         PlayerLife.InCharacterDeath += TriggerDead;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        //  Toggles the gravity depending on wheater the character is moving.
-        rb.gravityScale = (IsFalling)? 1:ActivateFriction();
-
         //  Gets the position where the screen is first touched.
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             startPos = Input.mousePosition;
         }
 
-        //  Gets the position where the screen is last touched.
-        if((Input.GetButtonUp("Fire1")) && (IsGrounded || DoubleSwipe) && !IsDead)
+        //  Let the character slash if IsGrounded (or gets the DoubleSwipe bonus) and not IsDead.
+        if ((Input.GetButtonUp("Fire1")) &&
+        (IsGrounded || DoubleSwipe) &&
+        !IsDead)
         {
-            //  Let the character slash if IsGrounded (or gets the DoubleSwipe bonus) and not IsDead.
-            Slash();
-        }
-    }
-
-    void Slash()
-    {
-            //  Starts the slash without any other force.
-            rb.velocity = Vector2.zero;
-
-            IsGrounded = false;
-            IsFalling = false;
+            //  Gets the position where the screen is last touched.
             finalPos = Input.mousePosition;
 
             //  Calculate the distance between x and y components, then traduces it to values from 0 to 1. 
@@ -63,20 +56,51 @@ public class PlayerMovement : MonoBehaviour
             //  Crops the Vector3 to Vector2
             DirectionSlash = new Vector2(v.x, v.y);
 
-            //  Impulses this object in slash's direction.
-            rb.AddForce(DirectionSlash * forceSwipe, ForceMode2D.Impulse);
+            //  Checks if the character IsStill...
+            if (IsStill)
+            {
+                //  if so, must slash upwards.
+                if (DirectionSlash.y > 0) Slash();
+            }
+            //  if not, slash anyway.
+            else Slash();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //  Toggles the gravity depending on wheater the character IsFalling.
+        rb.gravityScale = (IsFalling) ? 1 : ActivateFriction();
+    }
+
+    void Slash()
+    {
+        //  Starts the slash without any other force.
+        rb.velocity = Vector2.zero;
+
+        IsGrounded = false;
+        IsStill = false;
+        IsFalling = false;
+
+        //  Impulses this object in slash's direction.
+        rb.AddForce(DirectionSlash * forceSlash, ForceMode2D.Impulse);
+
+        //  Activate particle system.
+        PlayParticlesSlash();
+
+        //  ***Debug.Log(DirectionSlash);
     }
 
     int ActivateFriction()
     {
         //  Friction will be applied when character not IsGrounded.
-        if(!IsGrounded) 
+        if (!IsGrounded)
         {
             //  Applies a force contrary to DirectionSlash's direction.
-            rb.AddForce(-DirectionSlash * (forceSwipe * frictionSwipe), ForceMode2D.Force);
+            rb.AddForce(-DirectionSlash * (forceSlash * frictionSlash), ForceMode2D.Force);
 
             //  If the character loses all it's upping energy, start to fall.
-            if(rb.velocity.y <= 0) IsFalling = true;
+            if (rb.velocity.y <= 0) IsFalling = true;
         }
 
         return 0;
@@ -87,13 +111,16 @@ public class PlayerMovement : MonoBehaviour
         //  When touching a platform restart the Grab counter.
         StopAllCoroutines();
 
+        //  Check if the platform touched is floor.
+        if (collision.contacts[0].normal == Vector2.up) IsStill = true;
+
         //  Whenever touches a paltform stops and can slash again.
         rb.velocity = Vector2.zero;
         IsGrounded = true;
         IsFalling = false;
 
-        //  Activates Grab if the platform is not floor.
-        if(collision.contacts[0].normal != Vector2.up) StartCoroutine(Grab());
+        //  Activates Grab if the character not IsStill.
+        if (!IsStill) StartCoroutine(Grab());
     }
 
     //  When touches a platform doesn't fall de inmediaty.
@@ -104,8 +131,20 @@ public class PlayerMovement : MonoBehaviour
 
         IsFalling = true;
 
-        //  vvv Uncomment this line to make Slash unable Grab ends. vvv
+        //  vvv Uncomment this line to make Slash unable once Grab ends. vvv
         //IsGrounded = false;
+    }
+
+    void PlayParticlesSlash()
+    {
+        //  Calculate de arccos from negative X to get the angle and convert the result from radians to degrees.
+        float angleInRad = Mathf.Acos(-DirectionSlash.x) * Mathf.Rad2Deg;
+
+        //  Set te sense of the slash based on Y's value.
+        particleSlash.gameObject.transform.rotation = Quaternion.Euler((DirectionSlash.y < 0) ? -angleInRad : angleInRad,
+        90, 0);
+
+        particleSlash.Play();
     }
 
     void TriggerDead()
@@ -113,9 +152,10 @@ public class PlayerMovement : MonoBehaviour
         IsDead = true;
     }
 
-    public bool IsGrounded {get => canSwipe; set => canSwipe = value;}
-    public bool IsFalling {get => falling; set => falling = value;}
-    public bool IsDead {get; set;}
-    public bool DoubleSwipe {get => doubleSwipe; set => doubleSwipe = value;}
-    Vector2 DirectionSlash {get; set;}
+    public bool IsGrounded { get => canSwipe; set => canSwipe = value; }
+    public bool IsStill { get => onFloor; set => onFloor = value; }
+    public bool IsFalling { get => falling; set => falling = value; }
+    public bool IsDead { get; set; }
+    public bool DoubleSwipe { get => doubleSwipe; set => doubleSwipe = value; }
+    Vector2 DirectionSlash { get; set; }
 }
