63a23a255c661969b07c0082853d994585a69d80
JPVelandia <46972093+JPVelandia@users.noreply.github.com>
2020-08-24 23:09:51 -0500

    Menu and go to Tutorial scene

---
+++ Color Slasher/Assets/Scripts/HUD/Pause.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject[] assets;
+    static GameObject[] assetS;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        assetS = assets;
+        TrunOffAssets();
+    }
+  
+    public void GoMenu()
+    {
+        Debug.Log("Click");
+        SceneManager.LoadScene("Home");
+    }
+
+    public void TrunOnAssets()
+    {
+        foreach (GameObject obj in assetS)
+        {
+            obj.SetActive(true);
+        }
+
+        assetS[2].SetActive(false);
+    }
+
+    public void TrunOffAssets()
+    {
+        foreach (GameObject obj in assetS)
+        {
+            obj.SetActive(false);
+        }
+
+        assetS[2].SetActive(true);
+    }
+}
