c18d3604cd31df18846a8e30092a5109b205015b
PipeElPipe <felipe.arangoe@upb.edu.co>
2020-09-01 15:40:47 -0500

    ??

---
+++ Color Slasher/Assets/Scripts/EnemyCount.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCount : Character
+{
+    public int enemyCount = 0;
+    public Character character;
+
+    void Update()
+    {
+
+    }
+
+    /*protected override void TriggerIsDead()
+    {
+        base.
+    }*/
+}
