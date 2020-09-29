d4f9d5dfedea269e10ee9f1f83f8dd4f3db5af39
PipeElPipe <felipe.arangoe@upb.edu.co>
2020-09-29 01:11:43 -0500

    now it is not a bug

---
+++ Color Slasher/Assets/changeCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeCam : MonoBehaviour
+{
+    [SerializeField] GameObject cam1, cam2;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            cam2.SetActive(true);
+            cam1.SetActive(false);
+
+        }
+    }
+}
