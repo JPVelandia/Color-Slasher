4d754a5fa2a381fa6967f2b6742e9418f860a571
MariSerna371 <68366236+MariSerna371@users.noreply.github.com>
2020-10-30 13:47:09 -0500

    tutorial fail

---
+++ Color Slasher/Assets/Scripts/HUD/TutorialFunction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialFunction : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI TutText;
+    [SerializeField] GameObject TutStuff;
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+        TutStuff.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("BlueTut"))
+        {
+            TutText.text = "The blue power up allows you to double jump!";
+            TutStuff.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (collision.gameObject.CompareTag("YellowTut"))
+        {
+            TutText.text = "The yellow power up makes you able to slash faster!";
+            TutStuff.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (collision.gameObject.CompareTag("GreenTut"))
+        {
+            TutText.text = "The green power up helps you to heal up a bit!";
+            TutStuff.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (collision.gameObject.CompareTag("RedTut"))
+        {
+            TutText.text = "The red power up creates an explosion around you!";
+            TutStuff.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ClickOff()
+    {
+        TutStuff.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
